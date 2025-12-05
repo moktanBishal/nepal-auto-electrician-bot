2769b491406c5522e653862be06e9264c961329d
Bishal Lama <111524030+moktanBishal@users.noreply.github.com>
2025-12-05 20:58:35 +0545

    Update LLM model from gemini-1.5 to gemini-2.5

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if not api_key:
 
 # ====== LLM ======
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-2.5-flash",
     google_api_key=api_key,
     temperature=0.3
 )
